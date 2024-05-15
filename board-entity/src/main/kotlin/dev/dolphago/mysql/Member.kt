e53ddf41cdb59f61796fa826cce1cbed9c033345
DolphaGo <adamdoha@naver.com>
2024-05-15 17:54:46 +0900

    chore: Remove wildcard

--- board-entity/src/main/kotlin/dev/dolphago/mysql/Member.kt
+++ board-entity/src/main/kotlin/dev/dolphago/mysql/Member.kt
@@ -1,6 +1,14 @@
 package dev.dolphago.mysql
 
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.OneToMany
+import jakarta.persistence.Table
 
 @Entity
 @Table(name = "member")
@@ -27,7 +35,7 @@ data class Member(
 
     @Column(name = "role")
     @Enumerated(EnumType.STRING)
-    val role : Authority
+    val role: Authority
 ) : EntityListener() {
 
     @OneToMany(mappedBy = "member")
